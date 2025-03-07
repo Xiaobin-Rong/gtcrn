2622c8b9324a97b1017b4c22abf325afdaa71b12
Rong Xiaobin <xiaobin.rong@smail.nju.edu.cn>
2025-03-07 21:26:02 +0800

    Update convolution.py
    modify StreamConvTranspose2d to be more efficient

--- stream/modules/convolution.py
+++ stream/modules/convolution.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Dec  3 17:32:08 2022
-Modified on Tue Jan  9 17:47:18 2024
+Modified on Fri Mar  7 21:25:18 2025
 
 @author: Xiaohuai Le, Xiaobin Rong
 """
@@ -93,6 +93,79 @@ class StreamConv2d(nn.Module):
         return outp, out_cache
 
 
+## The inference of this implementation is slow, according to https://github.com/Xiaobin-Rong/gtcrn/issues/37
+# class StreamConvTranspose2d(nn.Module):
+#     def __init__(self, 
+#                  in_channels: int,
+#                  out_channels: int,
+#                  kernel_size: Union[int, Tuple[int, int]],
+#                  stride: Union[int, Tuple[int, int]] = 1,
+#                  padding: Union[str, int, Tuple[int, int]] = 0,
+#                  dilation: Union[int, Tuple[int, int]] = 1,
+#                  groups: int = 1,
+#                  bias: bool = True,
+#                  *args, **kargs):
+#         super().__init__(*args, **kargs)
+#         """
+#         kernel_size = [T_size, F_size] by default
+#         stride = [T_stride, F_stride] and assert T_stride == 1
+#         """
+#         if type(kernel_size) is int:
+#             self.T_size = kernel_size
+#             self.F_size = kernel_size
+#         elif type(kernel_size) in [list, tuple]:
+#             self.T_size, self.F_size = kernel_size
+#         else:
+#             raise ValueError('Invalid kernel size.')
+            
+#         if type(stride) is int:
+#             self.T_stride = stride
+#             self.F_stride = stride
+#         elif type(stride) in [list, tuple]:
+#             self.T_stride, self.F_stride = stride
+#         else:
+#             raise ValueError('Invalid stride size.')
+        
+#         assert self.T_stride == 1
+
+#         if type(padding) is int:
+#             self.T_pad = padding
+#             self.F_pad = padding
+#         elif type(padding) in [list, tuple]:
+#             self.T_pad, self.F_pad = padding
+#         else:
+#             raise ValueError('Invalid padding size.')
+        
+#         if type(dilation) is int:
+#             self.T_dilation = dilation
+#             self.F_dilation = dilation
+#         elif type(dilation) in [list, tuple]:
+#             self.T_dilation, self.F_dilation = dilation
+#         else:
+#             raise ValueError('Invalid dilation size.')
+        
+#         assert self.T_pad == (self.T_size-1) * self.T_dilation, "To meet the demands of causal streaming requirements"
+
+#         self.ConvTranspose2d = nn.ConvTranspose2d(in_channels = in_channels, 
+#                                                 out_channels = out_channels,
+#                                                 kernel_size = kernel_size,
+#                                                 stride = stride, 
+#                                                 padding = padding,
+#                                                 dilation = dilation,
+#                                                 groups = groups,
+#                                                 bias = bias)
+        
+#     def forward(self, x, cache):
+#         """
+#         x: [bs, C, 1, F]
+#         cache: [bs, C, T_size-1, F]
+#         """
+#         inp = torch.cat([cache, x], dim=2)
+#         outp = self.ConvTranspose2d(inp)
+#         out_cache = inp[:,:, 1:]
+#         return outp, out_cache
+
+
 class StreamConvTranspose2d(nn.Module):
     def __init__(self, 
                  in_channels: int,
@@ -107,8 +180,10 @@ class StreamConvTranspose2d(nn.Module):
         super().__init__(*args, **kargs)
         """
         kernel_size = [T_size, F_size] by default
-        stride = [T_stride, F_stride] and assert T_stride == 1
+        stride = [T_stride, F_stride], and T_stride == 1
         """
+        self.in_channels = in_channels
+        self.out_channels = out_channels
         if type(kernel_size) is int:
             self.T_size = kernel_size
             self.F_size = kernel_size
@@ -134,7 +209,8 @@ class StreamConvTranspose2d(nn.Module):
             self.T_pad, self.F_pad = padding
         else:
             raise ValueError('Invalid padding size.')
-        
+        assert(self.T_pad == 0) 
+
         if type(dilation) is int:
             self.T_dilation = dilation
             self.F_dilation = dilation
@@ -143,27 +219,48 @@ class StreamConvTranspose2d(nn.Module):
         else:
             raise ValueError('Invalid dilation size.')
         
-        assert self.T_pad == (self.T_size-1) * self.T_dilation, "To meet the demands of causal streaming requirements"
-
-        self.ConvTranspose2d = nn.ConvTranspose2d(in_channels = in_channels, 
-                                                out_channels = out_channels,
-                                                kernel_size = kernel_size,
-                                                stride = stride, 
-                                                padding = padding,
-                                                dilation = dilation,
-                                                groups = groups,
-                                                bias = bias)
+        # Implementing ConvTranspose2d using Conv2d with weight-time reversal.
+        self.ConvTranspose2d = nn.Conv2d(in_channels = in_channels, 
+                                        out_channels = out_channels,
+                                        kernel_size = kernel_size,
+                                        stride = (self.T_stride, 1), # An additional upsampling will be used in forward, if F_stride != 1
+                                        padding = (self.T_pad, 0),   # An additional padding will be used in forward, if F_pad != 0
+                                        dilation = dilation,
+                                        groups = groups,
+                                        bias = bias)
         
     def forward(self, x, cache):
         """
-        x: [bs, C, 1, F]
-        cache: [bs, C, T_size-1, F]
+        x: [bs,C,1,F]
+        cache: [bs,C,T-1,F]
         """
-        inp = torch.cat([cache, x], dim=2)
-        outp = self.ConvTranspose2d(inp)
-        out_cache = inp[:,:, 1:]
-        return outp, out_cache
+        # [bs,C,T,F]
+        inp = torch.cat([cache, x], dim = 2)
+        out_cache = inp[:, :, 1:]
+        bs, C, T, F = inp.shape
+        
+        # Upsampling operation
+        if self.F_stride > 1: 
+            # [bs,C,T,F] -> [bs,C,T,F,1] -> [bs,C,T,F,F_stride] -> [bs,C,T,F_out]
+            inp = torch.cat([inp[:,:,:,:,None], torch.zeros([bs,C,T,F,self.F_stride-1])], dim = -1).reshape([bs,C,T,-1])
+            left_pad = self.F_stride - 1
+            if self.F_size > 1:
+                if left_pad <= self.F_size - 1:
+                    inp = torch.nn.functional.pad(inp, pad = [(self.F_size - 1)*self.F_dilation-self.F_pad, (self.F_size - 1)*self.F_dilation-self.F_pad - left_pad, 0, 0])
+                else:
+                    # inp = torch.nn.functional.pad(inp, pad = [self.F_size - 1, 0, 0, 0])[:,:,:,: - (left_pad - self.F_stride + 1)]
+                    raise(NotImplementedError)
+            else:
+                # inp = inp[:,:,:,:-left_pad]
+                raise(NotImplementedError)
 
+        else: # F_stride = 1
+            inp = torch.nn.functional.pad(inp, pad=[(self.F_size-1)*self.F_dilation-self.F_pad, (self.F_size-1)*self.F_dilation-self.F_pad])
+                
+        outp = self.ConvTranspose2d(inp)
+    
+        return outp, out_cache
+    
 
 if __name__ == '__main__':
     from convert import convert_to_stream
